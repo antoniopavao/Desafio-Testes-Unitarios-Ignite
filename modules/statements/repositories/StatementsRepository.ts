9aa25eef9c80db9e1d23b7a262e01bd8c4b98319
Antonio Arauujo Pavao <antonioppavao@gmail.com>
2022-01-28 10:35:43 -0300

    Fixing CreateStatement to receive sender_id

--- modules/statements/repositories/StatementsRepository.ts
+++ modules/statements/repositories/StatementsRepository.ts
@@ -17,48 +17,69 @@ export class StatementsRepository implements IStatementsRepository {
     user_id,
     amount,
     description,
-    type
+    type,
+    sender_id,
   }: ICreateStatementDTO): Promise<Statement> {
     const statement = this.repository.create({
       user_id,
       amount,
       description,
-      type
+      type,
+      sender_id,
     });
 
     return this.repository.save(statement);
   }
 
-  async findStatementOperation({ statement_id, user_id }: IGetStatementOperationDTO): Promise<Statement | undefined> {
+  async findStatementOperation({
+    statement_id,
+    user_id,
+  }: IGetStatementOperationDTO): Promise<Statement | undefined> {
     return this.repository.findOne(statement_id, {
-      where: { user_id }
+      where: { user_id },
     });
   }
 
-  async getUserBalance({ user_id, with_statement = false }: IGetBalanceDTO):
-    Promise<
-      { balance: number } | { balance: number, statement: Statement[] }
-    >
-  {
+  async getUserBalance({
+    user_id,
+    with_statement = false,
+  }: IGetBalanceDTO): Promise<
+    { balance: number } | { balance: number; statement: Statement[] }
+  > {
     const statement = await this.repository.find({
-      where: { user_id }
+      where: [{ sender_id: user_id }, { user_id }],
     });
 
+    let num: Number;
+
     const balance = statement.reduce((acc, operation) => {
-      if (operation.type === 'deposit') {
-        return acc + operation.amount;
-      } else {
-        return acc - operation.amount;
+      switch (operation.type) {
+        case "withdraw":
+          num = Number(acc) - Number(operation.amount);
+          break;
+
+        case "deposit":
+          num = Number(acc) + Number(operation.amount);
+          break;
+
+        case "transfer":
+          if (operation.sender_id === user_id) {
+            num = Number(acc) - Number(operation.amount);
+          } else {
+            num = Number(acc) + Number(operation.amount);
+          }
+          break;
       }
-    }, 0)
+      return Number(num);
+    }, 0);
 
     if (with_statement) {
       return {
         statement,
-        balance
-      }
+        balance,
+      };
     }
 
-    return { balance }
+    return { balance };
   }
 }
--- modules/statements/useCases/createStatement/CreateStatementController.ts
+++ modules/statements/useCases/createStatement/CreateStatementController.ts
@@ -1,30 +1,43 @@
-import { Request, Response } from 'express';
-import { container } from 'tsyringe';
+import { Request, Response } from "express";
+import { container } from "tsyringe";
 
-import { CreateStatementUseCase } from './CreateStatementUseCase';
+import { CreateStatementUseCase } from "./CreateStatementUseCase";
 
 enum OperationType {
-  DEPOSIT = 'deposit',
-  WITHDRAW = 'withdraw',
+  DEPOSIT = "deposit",
+  WITHDRAW = "withdraw",
+  TRANSFER = "transfer",
 }
 
 export class CreateStatementController {
   async execute(request: Request, response: Response) {
     const { id: user_id } = request.user;
     const { amount, description } = request.body;
+    const { receiver_user } = request.params;
 
-    const splittedPath = request.originalUrl.split('/')
+    const splittedPath = request.originalUrl.split("/");
     const type = splittedPath[splittedPath.length - 1] as OperationType;
 
     const createStatement = container.resolve(CreateStatementUseCase);
 
-    const statement = await createStatement.execute({
-      user_id,
-      type,
-      amount,
-      description
-    });
+    let statement;
 
+    if (type === "deposit" || type === "withdraw") {
+      statement = await createStatement.execute({
+        user_id,
+        type,
+        amount,
+        description,
+      });
+    } else {
+      statement = await createStatement.execute({
+        user_id: receiver_user,
+        type: "transfer" as OperationType,
+        amount,
+        description,
+        sender_id: user_id,
+      });
+    }
     return response.status(201).json(statement);
   }
 }
