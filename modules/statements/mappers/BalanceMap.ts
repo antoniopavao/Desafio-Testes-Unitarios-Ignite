1be943ec95b8b97adc0e3fc78bdd1416ec65d791
Antonio Arauujo Pavao <antonioppavao@gmail.com>
2022-01-28 10:17:20 -0300

    Updating BalanceMap

--- modules/statements/mappers/BalanceMap.ts
+++ modules/statements/mappers/BalanceMap.ts
@@ -1,28 +1,48 @@
 import { Statement } from "../entities/Statement";
 
 export class BalanceMap {
-  static toDTO({statement, balance}: { statement: Statement[], balance: number}) {
-    const parsedStatement = statement.map(({
-      id,
-      amount,
-      description,
-      type,
-      created_at,
-      updated_at
-    }) => (
-      {
+  static toDTO({
+    statement,
+    balance,
+  }: {
+    statement: Statement[];
+    balance: number;
+  }) {
+    const parsedStatement = statement.map(
+      ({
         id,
-        amount: Number(amount),
+        amount,
         description,
         type,
         created_at,
-        updated_at
+        updated_at,
+        sender_id,
+      }) => {
+        if (type === "transfer") {
+          return {
+            id,
+            sender_id,
+            amount: Number(amount),
+            description,
+            type,
+            created_at,
+            updated_at,
+          };
+        }
+        return {
+          id,
+          amount: Number(amount),
+          description,
+          type,
+          created_at,
+          updated_at,
+        };
       }
-    ));
+    );
 
     return {
       statement: parsedStatement,
-      balance: Number(balance)
-    }
+      balance: Number(balance),
+    };
   }
 }
